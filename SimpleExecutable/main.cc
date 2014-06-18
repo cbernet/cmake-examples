039ba2d566911ba02900f0cb738417a4a21fa512
Colin <colin.bernet@cern.ch>
2014-06-18 12:58:17 +0200

    a few simple cmake examples

---
+++ SimpleExecutable/main.cc
@@ -0,0 +1,22 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// #include "hello.h"
+
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+    }
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+
+  // hello();
+  return 0;
+}
